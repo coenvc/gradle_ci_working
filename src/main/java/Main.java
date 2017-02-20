99508606253b965c739de3152d3f5f8e3b4fc6c7
Fhict <coenvc@gmail.com>
2017-02-20 15:08:03 +0100

    Working project

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+package main.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public boolean Test(){
+        return true;
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,15 @@
+package test.java;
+
+import main.java.Main;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by fhict on 20/02/2017.
+ */
+public class MainTest {
+    Main main = new Main();
+    @Test public void TestMain(){
+        Assert.assertEquals(main.Test(),true);
+    }
+}
